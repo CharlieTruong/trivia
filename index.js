f6f2d218130fcb36d3dd130d7e1a785e86c8c1d5
charlietruong <charlie.m.truong@gmail.com>
2019-03-02 15:52:18 -0600

    Initially fetch question

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const respond = (
   response,
   sessionAttributes = {},
@@ -20,16 +22,25 @@ exports.handler = async (event, context, callback) => {
       case "LaunchRequest":
         callback(null, respond("Welcome to comic book trivia"));
         break;
+      case "SessionEndedRequest":
+        callback(null, respond("See you next time"));
+        break;
       case "IntentRequest":
         const { name } = event.request.intent;
         switch (name) {
+          case "getQuestion":
+            const questionResponse = await axios.get(
+              "http://jsservice.io/random"
+            );
+            const question = questionResponse[0].data;
+            callback(null, respond("here is a question", question));
+            break;
+          case "answerQuestion":
+            break;
           default:
             callback(null, respond(`Intent type is ${name}`));
             break;
         }
-        break;
-      case "SessionEndedRequest":
-        callback(null, respond("See you next time"));
         break;
     }
   } catch (e) {
