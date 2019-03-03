a4860a84eab748784a89b3937487b399dfb3cab0
charlietruong <charlie.m.truong@gmail.com>
2019-03-02 20:39:52 -0600

    Add docstrings to lambda function

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+/**
+ * This modules defines a lambda function to handle the Alexa trivia skill
+ */
 const axios = require("axios");
 
+/**
+ * Returns an Alexa response object
+ *
+ * @param {string} response - the text response that Alexa should say
+ * @param {Object} sessionAttributes - any session params to pass back to the
+ *  Alexa device
+ * @param {boolean} shouldEndSession - whether the session should be ended
+ * @returns the Alexa response object
+ */
 const respond = (
   response,
   sessionAttributes = {},
@@ -16,33 +28,52 @@ const respond = (
   version: "1.0"
 });
 
+/**
+ * The lambda function that handles an Alexa event
+ *
+ * @param {Object} event - the event object that initiated the lambda. This
+ *  includes metadata on the event request type and intent.
+ * @param {Object} context - an object that provides methods about the
+ *  invocation and execution environment
+ * @param {Function} callback - the method to invoke to respond to the Alexa
+ *  event
+ */
 exports.handler = async (event, context, callback) => {
   try {
     switch (event.request.type) {
+      // Handle the initial invocation
       case "LaunchRequest": {
-        callback(null, respond("Welcome to comic book trivia"));
+        callback(null, respond("Welcome to Charlie's trivia game."));
         break;
       }
+
+      // Handle a session close request
       case "SessionEndedRequest": {
-        callback(null, respond("See you next time"));
+        callback(null, respond("See you next time."));
         break;
       }
+
+      // Handle Intents
       case "IntentRequest": {
         const { name, slots } = event.request.intent;
         switch (name) {
+          // Ask a question including the case where a user wants to play
+          // again after answering a previous question.
           case "AMAZON.YesIntent":
           case "getQuestion": {
-            let answered = false;
-            if (event.session.attributes) {
-              answered = event.session.attributes.answered;
-            }
+            const answered =
+              event.session.attributes && event.session.attributes.answered;
+            // Only handle a Yes Intent if the question was already answered
             if (name == "AMAZON.YesIntent" && !answered) {
               return;
             }
 
+            // Fetch a random question from jservice.io
             const questionResponse = await axios.get(
               "http://jservice.io/api/random"
             );
+
+            // Ask the question
             const { answer, category, question } = questionResponse.data[0];
             const questionText = `In the category "${
               category.title
@@ -53,6 +84,9 @@ exports.handler = async (event, context, callback) => {
             );
             break;
           }
+
+          // Check if the answer is correct and ask if the user wants to
+          // play again.
           case "answerQuestion": {
             const { answer } = event.session.attributes;
             if (answer.toLowerCase() === slots.answer.value.toLowerCase()) {
@@ -72,6 +106,8 @@ exports.handler = async (event, context, callback) => {
             }
             break;
           }
+
+          // If the user does not want to play again, then end the session.
           case "AMAZON.NoIntent": {
             if (
               event.session.attributes &&
